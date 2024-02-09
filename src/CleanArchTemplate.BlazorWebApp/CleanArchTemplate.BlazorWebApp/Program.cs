4429606363ad3705fc4b2e915f3fd4fd15f2725c
Mike Eckel <mikeeckel@hotmail.com>
2024-02-09 13:44:18 -0600

    Blazor Web App template updated template.json file updated

--- src/CleanArchTemplate.BlazorWebApp/CleanArchTemplate.BlazorWebApp/Program.cs
+++ src/CleanArchTemplate.BlazorWebApp/CleanArchTemplate.BlazorWebApp/Program.cs
@@ -1,34 +1,56 @@
-using CleanArchTemplate.BlazorWebApp.Components;
+#if (!UseServer && !UseWebAssembly)
+#endif
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
+#if (UseAuto)
     .AddInteractiveServerComponents()
     .AddInteractiveWebAssemblyComponents();
+#elif (UseServer)
+    .AddInteractiveServerComponents();
+#elif (UseWebAssembly)
+    .AddInteractiveWebAssemblyComponents();
+#endif
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+#if (UseWebAssembly)
 if (app.Environment.IsDevelopment())
 {
+#if (UseWebAssembly)
     app.UseWebAssemblyDebugging();
+#endif
 }
 else
+#else
+if (!app.Environment.IsDevelopment())
+#endif
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
 app.UseAntiforgery();
 
+#if (UseAuto)
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode()
     .AddInteractiveWebAssemblyRenderMode()
-    .AddAdditionalAssemblies(typeof(CleanArchTemplate.BlazorWebApp.Client._Imports).Assembly);
-
+#elif (UseServer)
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+#elif (UseWebAssembly)
+app.MapRazorComponents<App>()
+    .AddInteractiveWebAssemblyRenderMode()
+#else
+app.MapRazorComponents<App>();
+#endif
+#if (UseWebAssembly)
+  .AddAdditionalAssemblies(typeof(CleanArchTemplate.BlazorWebApp.Client._Imports).Assembly);
+#endif
 app.Run();
