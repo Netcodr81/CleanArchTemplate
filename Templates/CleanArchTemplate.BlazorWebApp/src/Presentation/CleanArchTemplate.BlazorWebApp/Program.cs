4d4f2a91c08734cc7c5be6dedfc56c00f0c05569
Mike Eckel <mikeeckel@hotmail.com>
2024-02-13 14:07:17 -0600

    Authorization code added to BlazorWebApp template

--- Templates/CleanArchTemplate.BlazorWebApp/src/Presentation/CleanArchTemplate.BlazorWebApp/Program.cs
+++ Templates/CleanArchTemplate.BlazorWebApp/src/Presentation/CleanArchTemplate.BlazorWebApp/Program.cs
@@ -1,4 +1,14 @@
 using CleanArchTemplate.BlazorWebApp.Components;
+#if (UseAuthorization && UseNone)
+using Microsoft.AspNetCore.Components.Server;
+#endif
+#if (UseAuthorization)
+using CleanArchTemplate.BlazorWebApp.Data;
+using CleanArchTemplate.BlazorWebApp.Components.Account;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+#endif
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,6 +26,39 @@ builder.Services.AddRazorComponents()
     .AddInteractiveWebAssemblyComponents();
 #elif (UseNone)
     builder.Services.AddRazorComponents();
+#endif
+
+#if (UseAuthorization)
+builder.Services.AddCascadingAuthenticationState();
+builder.Services.AddScoped<IdentityUserAccessor>();
+builder.Services.AddScoped<IdentityRedirectManager>();
+
+
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultScheme = IdentityConstants.ApplicationScheme;
+        options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
+    })
+    .AddIdentityCookies();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddSignInManager()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
+#endif
+
+#if (UseAuthorization && UseNone)
+builder.Services.AddScoped<AuthenticationStateProvider, ServerAuthenticationStateProvider>();
+#else
+builder.Services.AddScoped<AuthenticationStateProvider, PersistingRevalidatingAuthenticationStateProvider>();
 #endif
 
 var app = builder.Build();
@@ -56,5 +99,9 @@ app.MapRazorComponents<App>();
 #endif
 #if (UseWebAssembly)
   .AddAdditionalAssemblies(typeof(CleanArchTemplate.BlazorWebApp.Client._Imports).Assembly);
+#endif
+
+#if (UseAuthorization)
+app.MapAdditionalIdentityEndpoints();
 #endif
 app.Run();
