325fe5e5255a46b5955b8aec44d09d57ae815c9c
Mike Eckel <mikeeckel@hotmail.com>
2024-02-15 10:37:52 -0600

    Presentation project for MVC Template recreated.

---
+++ Templates/CleanArchTemplate.MVC/src/Presentation/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CleanArchTemplate.MVC.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
